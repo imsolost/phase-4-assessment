ba5da0a9ac5989ffaab16c5115d86554cd376e2b
Ryan Kent <imsolost@gmail.com>
2017-09-06 11:24:13 -0700

    styling corrections

--- public/script.js
+++ public/script.js
@@ -1,16 +1,5 @@
 console.log('hello from the browser JavaScript')
 
-const emptyCheck = () => {
-  const inputs = document.querySelectorAll('.form-input')
-
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value === '') {
-      alert('input fields cannot be blank')
-      return false
-    }
-  }
-}
-
 const deletePopup = () => {
   if (confirm('Are you sure you want to delete this post?')) {
     fetch(`/delete/${event.target.id}`, {
